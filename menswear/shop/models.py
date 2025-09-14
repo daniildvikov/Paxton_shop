382b4412ab8ea08c842d3b14a916b61f8b74b2f6
daniildvikov <danillafuture@gmail.com>
2025-09-14 20:20:45 +0300

    Initialize Django project structure with basic settings, models, views, and templates

---
+++ menswear/shop/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, verbose_name="Название")
+    description = models.TextField(verbose_name="Описание", blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
+    image = models.ImageField(upload_to='products/', blank=True, null=True, verbose_name="Изображение")
+
+    def __str__(self):
+        return self.name
